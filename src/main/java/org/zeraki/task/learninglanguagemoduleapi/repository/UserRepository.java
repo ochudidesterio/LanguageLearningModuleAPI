60be9f49c8a877b5d3d8022147e88d0a09fe2ef9
ochudidesterio <desochudi@gmail.com>
2024-05-24 06:27:37 +0300

    UserDetails implentation to fetch user from databse for authentication

--- src/main/java/org/zeraki/task/learninglanguagemoduleapi/repository/UserRepository.java
+++ src/main/java/org/zeraki/task/learninglanguagemoduleapi/repository/UserRepository.java
@@ -1,10 +1,2 @@
-package org.zeraki.task.learninglanguagemoduleapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zeraki.task.learninglanguagemoduleapi.models.user.AppUser;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<AppUser,Long> {
-    Optional<AppUser>findByUsername(String username);
+package org.zeraki.task.learninglanguagemoduleapi.repository;public interface UserRepository {
 }
